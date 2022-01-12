770ba0da650733ba22b82d40f3d2b15e7ed711e8
afkan_guseynov <af.kan@bk.ru>
2022-01-12 22:27:56 +0500

    commit

---
+++ src/main/java/src/lambda/OptionalIfPresent.java
@@ -0,0 +1,23 @@
+package src.lambda;
+
+import java.util.Optional;
+
+public class OptionalIfPresent {
+    public static void ifPresent(int[] data) {
+        max(data).ifPresent(s -> System.out.println("Max: " + s));
+    }
+
+    private static Optional<Integer> max(int[] data) {
+        //Если массив пуст, то возвращает пустой Optional
+        if (data.length == 0) {
+            return Optional.empty();
+        }
+        int max = data[0];
+        for (int i = 0; i < data.length; i++) {
+            if (max < data[i]) {
+                max = data[i];
+            }
+        }
+        return Optional.of(max);
+    }
+}
