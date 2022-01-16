f931cb8f9c58c0b641586ca2edb4861b6d23bcad
afkan_guseynov <af.kan@bk.ru>
2022-01-16 17:05:16 +0500

    1.1. Predicate. Проверить, что число положительное

---
+++ src/test/java/src/lambda/PredicateCheckPositiveTest.java
@@ -0,0 +1,15 @@
+package src.lambda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PredicateCheckPositiveTest {
+    @Test
+    public void whenPositive() {
+        assertTrue(PredicateCheckPositive.check(1));
+        assertFalse(PredicateCheckPositive.check(-1));
+        assertFalse(PredicateCheckPositive.check(0));
+    }
+}
