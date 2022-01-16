0bf1eb55c0378de57aaf2605fbe6ebe381244b0d
afkan_guseynov <af.kan@bk.ru>
2022-01-16 17:26:36 +0500

    1.3. Predicate. Проверить, что число положительное и четное

---
+++ src/test/java/src/lambda/PredicateCheckEvenAndPositiveTest.java
@@ -0,0 +1,16 @@
+package src.lambda;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PredicateCheckEvenAndPositiveTest {
+    @Test
+    public void test() {
+        assertTrue(PredicateCheckEvenAndPositive.check(2));
+        assertFalse(PredicateCheckEvenAndPositive.check(-2));
+        assertFalse(PredicateCheckEvenAndPositive.check(1));
+    }
+
+}
