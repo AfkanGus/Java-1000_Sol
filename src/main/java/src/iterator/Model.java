9faa5e5f4f9994f411c70e1803b38bb9f8bd5ace
afkan_guseynov <af.kan@bk.ru>
2024-04-06 17:55:35 +0500

    3. Утверждения с примитивными типами [#504883 #344295]

---
+++ src/main/java/src/iterator/Model.java
@@ -0,0 +1,32 @@
+package src.iterator;
+//модель для написания Asser утверждения.
+//3. Утверждения с примитивными типами [#504883 #344295]
+public class Model {
+    private int top;
+    private double num;
+    private String line;
+    private boolean condition;
+
+    public Model(int top, double num, String line, boolean condition) {
+        this.top = top;
+        this.num = num;
+        this.line = line;
+        this.condition = condition;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public double getNum() {
+        return num;
+    }
+
+    public String getLine() {
+        return line;
+    }
+
+    public boolean isCondition() {
+        return condition;
+    }
+}
--- src/test/java/src/iterator/ArrayItTest.java
+++ src/test/java/src/iterator/ArrayItTest.java
@@ -1,25 +1,28 @@
 package src.iterator;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ArrayItTest {
     @Test
     void whenMultiCallHasNextThenTrue() {
+        //Arrange - подготовка данных для проверки.
         ArrayIt iterator = new ArrayIt(
-                new int[] {1, 2, 3}
+                new int[]{1, 2, 3}
         );
+        //Act - выполнения действия и сохранения результата.
         boolean result = iterator.hasNext();
+        // Assert - оценка результат ожидаемому исходу.
         assertThat(result).isTrue();
+        // этапы размещены в одной строке.
         assertThat(iterator.hasNext()).isTrue();
     }
 
     @Test
     void whenReadSequence() {
         ArrayIt iterator = new ArrayIt(
-                new int[] {1, 2, 3}
+                new int[]{1, 2, 3}
         );
         assertThat(iterator.next()).isEqualTo(1);
         assertThat(iterator.next()).isEqualTo(2);
